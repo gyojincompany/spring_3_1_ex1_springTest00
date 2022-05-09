bd2b44b3840fea26e75b28771805ed092174b97e
JBEDU000 <JBEDU000@JBEDU00>
2022-05-09 11:20:34 +0900

    DI 코드로 변경

--- src/main/java/com/gyojincompany/spring_3_1/MainClass.java
+++ src/main/java/com/gyojincompany/spring_3_1/MainClass.java
@@ -1,17 +1,25 @@
 package com.gyojincompany.spring_3_1;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		MyCalculator myCal = new MyCalculator();
-		Calculator cal = new Calculator();
+//		MyCalculator myCal = new MyCalculator();
+//		Calculator cal = new Calculator();
+//		
+//		myCal.setCalcu(cal);//멤버객체 calcu 초기화;
+//		
+//		myCal.setA(100);//멤버변수 a 초기화
+//		myCal.setB(200);//멤버변수 b 초기화
 		
-		myCal.setCalcu(cal);//멤버객체 calcu 초기화;
-		
-		myCal.setA(10);//멤버변수 a 초기화
-		myCal.setB(20);//멤버변수 b 초기화
+		String configLocation = "classpath:applicationCTX.xml";		
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		//컨테이너 설정 및 ctx 생성
+		MyCalculator myCal = ctx.getBean("myCal", MyCalculator.class);//컨테이너에서 빈(객체) 주입
 		
 		myCal.add();
 		
